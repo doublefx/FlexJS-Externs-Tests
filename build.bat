d9fe40e5d1d3da450c6253275a9f5df5d65392af
Frédéric THOMAS <webdoublefx@gmail.com>
2015-07-01 02:37:28 +0100

    CAN BUILD THRU THE COMMAND LINE
    Cleaned up

--- build.bat
+++ build.bat
@@ -9,7 +9,4 @@ java -cp "%FALCON_CLASSPTH%" -Dflexlib="%FLEX_HOME%\frameworks" flex2.tools.Comp
 
 java -cp "%FALCON_CLASSPTH%" -Dflexlib="%FLEX_HOME%\frameworks" flex2.tools.Compc -external-library-path=%FALCON_HOME%/externs/js/out/bin/js.swc -define+=COMPILE::AS3,true -define+=COMPILE::JS,false -include-file=js/out/*,%PROJECT_HOME%/bin/HtmlContainer/js/out -source-path=%PROJECT_HOME%/HtmlContainer/src -include-classes HtmlContainer -output=%PROJECT_HOME%/bin/HtmlContainer\HtmlContainer.swc
 
-java -cp "%FALCON_CLASSPTH%" -Dflexlib="%FLEX_HOME%\frameworks" flex2.tools.Mxmlc -load-config=%FLEX_HOME%/frameworks/flex-config.xml -closure-lib=%CLOSURE_LIBRARY% -sdk-js-lib=%FLEX_HOME%/frameworks/js/FlexJS/libs -external-library-path=%FALCON_HOME%/externs/js/out/bin/js.swc -external-library-path=%FALCON_HOME%/externs/js/out/bin/js.swc -external-library-path=%FALCON_HOME%/externs/jquery/out/bin/jquery-1.9.swc -library-path=%PROJECT_HOME%/bin/HtmlContainer\HtmlContainer.swc -js-output-type=FLEXJS %PROJECT_HOME%/src/Main.as -output=%PROJECT_HOME%/bin/ExternsApp
-
-
-#to debug it: -Dagentlib:jdwp=transport=dt_shmem,server=n,address=javadebug,suspend=y
+java -cp "%FALCON_CLASSPTH%" -Dflexlib="%FLEX_HOME%\frameworks" flex2.tools.Mxmlc -load-config=%FLEX_HOME%/frameworks/flex-config.xml -closure-lib=%CLOSURE_LIBRARY% -sdk-js-lib=%FLEX_HOME%/frameworks/js/FlexJS/libs -external-library-path=%FALCON_HOME%/externs/js/out/bin/js.swc -external-library-path=%FALCON_HOME%/externs/jquery/out/bin/jquery-1.9.swc -library-path=%PROJECT_HOME%/bin/HtmlContainer\HtmlContainer.swc -js-output-type=FLEXJS -output=%PROJECT_HOME%/bin/ExternsApp %PROJECT_HOME%/src/Main.as
